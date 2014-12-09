64ec716ed58d476fc18f2e600ec3dd52b33996a3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-09 07:34:14 +0100

    + .ps1

---
+++ $env.ps1
@@ -0,0 +1,8 @@
+#
+#     Expand environment variables by prepending
+#     them with a $env:
+#
+write-output ""
+write-output "userprofile = $env:userprofile"
+write-output "home        = $env:home"
+write-output ""
