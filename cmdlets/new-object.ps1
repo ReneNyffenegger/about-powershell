4fb4aa6175b2bd2fe8f12925b946122dae8e5eed
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-15 10:35:47 +0100

    + new-object

---
+++ cmdlets/new-object.ps1
@@ -0,0 +1,12 @@
+$obj = new-object -typeName PSObject
+
+$obj | get-member
+#
+#    TypeName: System.Management.Automation.PSCustomObject
+# 
+# Name        MemberType Definition
+# ----        ---------- ----------
+# Equals      Method     bool Equals(System.Object obj)
+# GetHashCode Method     int GetHashCode()
+# GetType     Method     type GetType()
+# ToString    Method     string ToString()
