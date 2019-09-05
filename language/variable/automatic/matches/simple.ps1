cc096e2c9655b1b9c6478cbf95704041a5d581ce
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-05 14:11:07 +0200

    Update simple.ps1

--- language/variable/automatic/matches/simple.ps1
+++ language/variable/automatic/matches/simple.ps1
@@ -1,3 +1,9 @@
 'Foo 42 bar 99 baz' -match '\d+' | out-null
-
 write-host "The matched number is $($matches[0])."
+#
+#  The matched number is 42. 
+
+'Check 999 too' -notmatch '\d+' | out-null
+write-host "The matched number is $($matches[0])."
+#
+#  The matched number is 999.
