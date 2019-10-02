7d0e6c8b8d04eeedccd8fa8eb1c1738f71f3a3ce
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-10-02 13:39:36 +0200

    example with regular expressions

--- language/operator/string-manipulation/replace.ps1
+++ language/operator/string-manipulation/replace.ps1
@@ -1,12 +1,9 @@
-#
 #  Replace in ONE string
 #
 'foo, bar and baz' -replace 'ba', 'xyz'
 #
 # foo, xyzr and xyzz
-#
 
-#
 #  Replace in collection of strings
 #
 'foo', 'bar', 'and', 'baz' -replace 'ba', 'xyz'
@@ -15,4 +12,12 @@
 # xyzr
 # and
 # xyzz
+
+#  Replace with regular expressions.
+#  Two adjacent equal letters are replaced
+#  with the number 2 and the letter:
 #
+'apple', 'pear', 'hello' -replace '(.)\1', '2$1' -join ', '
+#
+# a2ple, pear, he2lo
+
