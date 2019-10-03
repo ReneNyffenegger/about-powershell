2493f85e26d3fb6e41ec4644213d76a6e46a7139
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-10-03 15:55:49 +0200

    Empty lines are ignored...

--- cmdlets/stringData/convertFrom/multiLine.ps1
+++ cmdlets/stringData/convertFrom/multiLine.ps1
@@ -1,6 +1,11 @@
 $data = @'
 foo = 42
 bar = 99
+
+# Empty lines and lines
+# starting with the comment
+# character are ignored.
+
 baz =  0
 '@
 
@@ -9,3 +14,11 @@ $ht = convertFrom-stringData $data
 write-output "Value of bar = $($ht['bar'])"
 #
 #  Value of bar = 99
+
+write-output $ht
+#
+# Name                           Value
+# ----                           -----
+# baz                            0
+# bar                            99
+# foo                            42
