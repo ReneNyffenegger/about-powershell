6c85f6210e8022e5e870010456cb22e0652ae143
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-10-16 08:45:57 +0200

    + out-gridView

---
+++ cmdlets/gridView/out/array-psCustomObjects.ps1
@@ -0,0 +1,3 @@
+[psCustomObject] @{ num = 1; english = 'one'  ; german = 'eins'; french = 'un'    },
+[psCustomObject] @{ num = 2; english = 'two'  ; german = 'zwei'; french = 'deux'  },
+[psCustomObject] @{ num = 3; english = 'three'; german = 'drei'; french = 'trois' } | out-gridView
---
+++ cmdlets/gridView/out/array.ps1
@@ -0,0 +1,1 @@
+'foo', 'bar', 'baz' | out-gridView -title 'An array'
