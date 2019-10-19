882df98d1b29ef81f00b85820ccc839a77a809f0
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-10-19 21:44:20 +0200

    + int-array.ps1

---
+++ cmdlets/object/measure/int-array.ps1
@@ -0,0 +1,9 @@
+4,6,3,7 | measure-object -minimum -maximum -average -sum
+#
+# Count             : 4
+# Average           : 5
+# Sum               : 20
+# Maximum           : 7
+# Minimum           : 3
+# StandardDeviation :
+# Property          :
