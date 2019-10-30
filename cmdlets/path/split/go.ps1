e719aa94e252bf161266fc7c5f3bfc7808c4f313
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-10-30 15:53:53 +0100

    more examples

--- cmdlets/path/split/go.ps1
+++ cmdlets/path/split/go.ps1
@@ -1,25 +1,43 @@
 $path = 'drive:\dirOne\dirTwo\file.ext'
 
+#  Get directory that contains the path:
+#
 split-path $path
 #
 #  drive:\dirOne\dirTwo
 
+#  Get file name only:
+#
 split-path $path -leaf
 #
 #  file.ext
 
+#  Get file name without extension:
+#
 split-path $path -leafBase
 #
 #  file
 
+#  Check if path is absolute:
+#
 split-path $path -isAbsolute
 #
 #  True
 
+#  Get drive of path:
+#
 split-path $path -qualifier
 #
 #  drive:
 
+#  Get entire path without drive:
+#
 split-path $path -noQualifier
 #
 #  \dirOne\dirTwo\file.ext
+
+#  Get parent's directory parent directory
+#
+split-path (split-path $path -parent) -parent
+#
+#  drive:\dirOne
