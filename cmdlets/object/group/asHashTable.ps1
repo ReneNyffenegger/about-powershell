526dcb96fd2cb5a91f815f001d6d3dedb7ca8edd
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-16 01:43:30 +0100

    print data type

--- cmdlets/object/group/asHashTable.ps1
+++ cmdlets/object/group/asHashTable.ps1
@@ -2,6 +2,11 @@ $data = import-CSV data.csv
 
 $quarters = $data | group-object -asHashTable quarter
 
+$quarters.GetType().FullName
+#
+#  System.Collections.Hashtable
+#
+
 ( $quarters.Q3 | where-object item -eq baz ).val
 #
 #  16
