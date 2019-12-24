8cbd76598fe934a3e1f0e6d42346a41ef2dee61b
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-24 20:48:23 +0100

    add bar

--- cmdlets/job/start/pass-variable.ps1
+++ cmdlets/job/start/pass-variable.ps1
@@ -1,6 +1,6 @@
 $var=42
 
-$scriptBlock = { add-content ~/variable-value "var=$var using:var=$using:var" }
+$scriptBlock = { add-content ~/variable-value "var=$var | using:var=$using:var" }
 
 $var='changed'
 
