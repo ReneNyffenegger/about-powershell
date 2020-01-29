06a3a79d17f20f441b546ebfd4e26f2501eaf949
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-01-29 15:48:06 +0100

    + Message Box

---
+++ examples/message-box/user32-MessageBox.ps1
@@ -0,0 +1,24 @@
+add-type -typeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+ 
+public static partial class user32 {
+
+   [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern Int32 MessageBox(
+            IntPtr hWnd,
+            String text,
+            String caption,
+            int    options
+    );
+}
+"@
+ 
+$chosen = [user32]::MessageBox(0, 'Yes or no?' , 'Choose wisely', 4) # 4: MB_OKCANCEL
+
+if ($chosen -eq 6) { # 6: ID_YES
+   write-host "Yeah!"
+}
+else {
+   write-host "Nay!"
+}
