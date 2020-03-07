2de17effe6259d61e4a9d13b203ff3d5674de235
René Nyffenegger <mail@renenyffenegger.ch>
2020-03-07 19:06:18 +0100

    + IEnumerator.ps1

---
+++ language/statement/foreach/IEnumerator.ps1
@@ -0,0 +1,30 @@
+class tq84Enumerator: System.Collections.IEnumerator {
+
+   [int] $val
+
+   tq84Enumerator() {
+       $this.Reset();
+   }
+
+   [object] get_Current() {
+      return $this.val
+   }
+
+   [bool] MoveNext() {
+      $this.val++
+
+      if ($this.val -gt 13) {
+         return $false
+      }
+      return $true
+   }
+
+   [void] Reset() {
+       $this.val = 7;
+   }
+}
+
+$e = [tq84Enumerator]::new()
+foreach ($f in $e) {
+  $f
+}
