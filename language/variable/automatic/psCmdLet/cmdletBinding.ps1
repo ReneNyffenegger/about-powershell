8a44d13a8be573f0f1ca260badb997e287112be1
Rene Nyffenegger <mail@renenyffenegger.ch>
2020-03-16 08:31:14 +0100

    + [parameter()]

--- language/variable/automatic/psCmdLet/cmdletBinding.ps1
+++ language/variable/automatic/psCmdLet/cmdletBinding.ps1
@@ -1,30 +1,44 @@
 #
-#  Define a function
+#  Defining a few functions
 #
-function func_ordinary {
-  param($param)
-  write-output "psCmdlet = $psCmdlet, param = $param"
+function func_1 {
+   param($param)
+   write-output "psCmdlet = $psCmdlet, param = $param"
 }
 
 #
 #  Define another function with the only difference
 #  that is has the cmdletBinding attribute:
 #
-function func_cmdlet {
+function func_2 {
   [cmdletBinding()]
-  param($param)
-  write-output "psCmdlet = $psCmdlet, param = $param"
+   param($param)
+   write-output "psCmdlet = $psCmdlet, param = $param"
 }
+
+#
+#  This function does not have a a [cmdletBinding()] instruction.
+#  However, the para() statement encloses a [parameter(...)] thingy
+#  which also causes $psCmdlet to be defined:
+#
+function func_3 {
+   param([parameter()] [string] $param)
+   write-output "psCmdlet = $psCmdlet, param = $param"
+}
+
 
 #
 #  Invoking the functions
 #
 
-func_ordinary one
+func_1 one
 #
 #  psCmdlet = , param = one
 
-func_cmdlet   two
+func_2   two
 #
 #  psCmdlet = System.Management.Automation.PSScriptCmdlet, param = two
 
+func_3 three
+#
+#  psCmdlet = System.Management.Automation.PSScriptCmdlet, param = three
