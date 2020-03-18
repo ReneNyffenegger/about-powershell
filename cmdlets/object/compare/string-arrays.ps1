3fabd0d008765589f1b1ee54baec11e2a948e7cd
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-03-18 21:57:36 +0100

    + string-arrays.ps1

---
+++ cmdlets/object/compare/string-arrays.ps1
@@ -0,0 +1,9 @@
+$words_1 =        'foo', 'bar', 'baz', 'four'
+$words_2 = 'one', 'foo', 'bar', 'baz'
+
+compare-object $words_1  $words_2
+#
+#  InputObject SideIndicator
+#  ----------- -------------
+#  one         =>
+#  four        <=
