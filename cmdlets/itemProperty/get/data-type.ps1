b7b55bd8c8377dcede46666f682cf028d15f1b9a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-06-10 08:54:29 +0200

    add directory and file

--- cmdlets/itemProperty/get/data-type.ps1
+++ cmdlets/itemProperty/get/data-type.ps1
@@ -1,18 +1,36 @@
-$propKey = get-itemProperty 'HKCU:\Control Panel\International'
-$propKey.GetType().FullName
+$scriptName = $myInvocation.myCommand.name
+$propFile = get-itemProperty $scriptName
+$propFile.GetType().FullName
+#
+#   System.IO.FileInfo
+
+$propFileattr = get-itemProperty $scriptName lastAccessTime
+$propFileattr.GetType().FullName
+#
+#   System.Management.Automation.PSCustomObject
+
+
+$propDir = get-itemProperty .
+$propDir.GetType().FullName
+#
+#   System.IO.DirectoryInfo
+
+
+$propRegKey = get-itemProperty 'HKCU:\Control Panel\International'
+$propRegKey.GetType().FullName
 #
 #   System.Management.Automation.PSCustomObject
 
 $propVal = get-itemProperty 'HKCU:\Control Panel\International' sTimeFormat
 $propVal.GetType().FullName
 #
-#    System.Management.Automation.PSCustomObject
+#   System.Management.Automation.PSCustomObject
 
 $propVal.sTimeFormat.GetType().FullName
 #
-#  System.String
+#   System.String
 
 $val = get-itemPropertyValue 'HKCU:\Control Panel\International' sTimeFormat
 $val.GetType().FullName
 #
-#  System.String
+#   System.String
