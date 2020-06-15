468fb0e301bcc581b11b291bdf8af1e55dce28c1
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-06-15 14:53:52 +0200

    + enumerator.py

---
+++ language/statement/for/enumerate.py
@@ -0,0 +1,15 @@
+things = [ 'foo', 'bar', 'baz' ]
+
+for item in things:
+    print(item)
+#    
+#  foo
+#  bar
+#  baz
+
+for index, item in enumerate(things):
+    print(index, item)
+#
+#  0 foo
+#  1 bar
+#  2 baz
