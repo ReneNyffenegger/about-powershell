ddc5baf5b8671c6f5753bf40870137ffe590c31a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-01 20:36:16 +0100

    more comments

--- language/type/array/plus-vs-comma.ps1
+++ language/type/array/plus-vs-comma.ps1
@@ -4,6 +4,9 @@ $abc.length
 # 3
 
 # ----- Create an array with the comma operator
+#
+#       $abc is turned into one element so that
+#       the resulting array consists of 4 elements:
 
 $ary = $abc, 'd', 'e', 'f'
 $ary.length
@@ -18,11 +21,15 @@ $ary | foreach-object { "$_" }
 # f
 
 # ----- Create an array with the plus operator
+#
+#       The elements in $abc are exploded/expanded
+#       so that the resulting array consists
+#       of 6 elements:
 
 $ary = $abc + 'd', 'e', 'f'
 $ary.length
 #
-# 4
+# 6
 
 $ary | foreach-object { "$_" }
 #
