f2137e65406340ffa0a9abc236056aa37b1fc1dc
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-03 21:43:12 +0100

    + array-subexpression

---
+++ language/operator/array-subexpression/array-from-funcs.ps1
@@ -0,0 +1,11 @@
+$ary_0 = @( func_0 )
+$ary_1 = @( func_1 )
+$ary_n = @( func_n )
+
+$ary_0.GetType().FullName
+$ary_1.GetType().FullName
+$ary_n.GetType().FullName
+
+$ary_0.length
+$ary_1.length
+$ary_n.length
---
+++ language/operator/array-subexpression/funcs.ps1
@@ -0,0 +1,3 @@
+function func_0 {                     }
+function func_1 { 'one element '      }
+function func_n { 'foo', 'bar', 'baz' }
