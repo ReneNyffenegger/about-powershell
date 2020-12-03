2c84243e48972dd8b0658b03dda00a64041b1376
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-03 21:57:16 +0100

    show result

--- language/operator/array-subexpression/array-from-funcs.ps1
+++ language/operator/array-subexpression/array-from-funcs.ps1
@@ -2,10 +2,10 @@ $ary_0 = @( func_0 )
 $ary_1 = @( func_1 )
 $ary_n = @( func_n )
 
-$ary_0.GetType().FullName
-$ary_1.GetType().FullName
-$ary_n.GetType().FullName
+$ary_0.GetType().FullName  # System.Object[]
+$ary_1.GetType().FullName  # System.Object[]
+$ary_n.GetType().FullName  # System.Object[]
 
-$ary_0.length
-$ary_1.length
-$ary_n.length
+$ary_0.length              # 0
+$ary_1.length              # 1
+$ary_n.length              # 3
