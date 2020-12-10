30f99b3ceeeb53f8dda69de9180d75d170883511
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-10 22:11:30 +0100

    Use C:\Users\Rene\github\about\PowerShell\language\statement\function\parameters\attributes\validate\Script

--- language/statement/function/parameters/attributes/validate/Script/ensure-directory.ps1
+++ language/statement/function/parameters/attributes/validate/Script/ensure-directory.ps1
@@ -1,7 +1,7 @@
 param (
-  [parameter     (    mandatory=$true                    )] # make sure paramter exists
-  [validateScript( { [System.IO.Directory]::Exists($_) } )] # make sure path exists
-  [string                                                 ]   $path
+  [parameter     (    mandatory=$true                           )] # make sure paramter exists
+  [validateScript( { [System.IO.Directory]::Exists("$pwd/$_") } )] # make sure path exists
+  [string                                                        ]   $path
 )
 
 ls $path
