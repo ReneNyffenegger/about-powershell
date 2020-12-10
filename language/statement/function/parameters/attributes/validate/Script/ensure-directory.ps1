68af9d002b7beb8e5fce2e47765f85a817343ee8
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-10 21:46:29 +0100

    align parentheses

--- language/statement/function/parameters/attributes/validate/Script/ensure-directory.ps1
+++ language/statement/function/parameters/attributes/validate/Script/ensure-directory.ps1
@@ -1,6 +1,6 @@
 param (
-  [parameter     (    mandatory=$True)                    ] # make sure paramter exists
-  [validateScript( { [System.IO.Directory]::Exists($_) }) ] # make sure path exists
+  [parameter     (    mandatory=$true                    )] # make sure paramter exists
+  [validateScript( { [System.IO.Directory]::Exists($_) } )] # make sure path exists
   [string                                                 ]   $path
 )
 
