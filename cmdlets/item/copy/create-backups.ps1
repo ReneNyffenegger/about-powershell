841a35bd7d4ca725f0f417f0e4deeaf365309fe3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-19 16:28:34 +0100

    txt ->ps1

--- cmdlets/item/copy/create-backups.ps1
+++ cmdlets/item/copy/create-backups.ps1
@@ -1,4 +1,4 @@
-get-childItem -recurse *.txt |
+get-childItem -recurse *.ps1 |
   foreach-object {
-     copy-item  $_  $( $_.fullName -replace 'txt$', 'txt.bak' )
+     copy-item  $_  $( $_.fullName -replace 'ps1$', 'ps1.bak' )
   }
