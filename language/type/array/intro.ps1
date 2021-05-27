02c6dbac153e18fdff8253f1cfd0f3ece267ef11
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-05-27 02:01:38 +0200

    iterate over array using foreach or foreach-object

--- language/type/array/intro.ps1
+++ language/type/array/intro.ps1
@@ -1,4 +1,5 @@
 $ary = 'foo', 'bar', 'baz'
+
 write-output "First element is:  $($ary[0])"
 write-output "Second element is: $($ary[1])"
 write-output "Last element is:   $($ary[-1])"
@@ -8,3 +9,11 @@ write-output "The array has $($ary.count) elements"
 # Second element is: bar
 # Last element is:   baz
 # The array has 3 elements
+
+# Iterating over the array using the foreach-object cmdlet …
+$ary | foreach-object { $_.ToUpper() }
+
+# … or the foreach statement: 
+foreach ($elem in $ary) {
+   $elem.ToUpper() 
+}
