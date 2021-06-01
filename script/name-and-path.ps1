772d3f38bcb7c7bd2fc5d97e1bd7f3d2a6e9bf93
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-06-01 08:51:14 +0200

    add myInvocation.myCommand.path

--- script/name-and-path.ps1
+++ script/name-and-path.ps1
@@ -1,8 +1,11 @@
 set-strictMode -version latest
 
 write-host
-write-host "  The name of script's file is:             $($myInvocation.myCommand.name)"
-write-host "  It is located in the directory:           $psScriptRoot"
+write-host "  The name of script's file is            : $($myInvocation.myCommand.name)"
+write-host "  It is located in the directory          : $psScriptRoot"
 write-host
 write-host "  Thus, the complete path to the script is: $psCommandPath"
+write-host
+write-host "  The directory can also be evaluated with"
+write-host "  `$myInvocation.myCommand.path            : $($myInvocation.myCommand.path)"
 write-host
