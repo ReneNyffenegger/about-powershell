9def62c851d73c7a842aa7f0cd3eb0f508a0f0f9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-06-13 17:52:39 +0200

    + objects.ps1

---
+++ language/operator/comparison/objects.ps1
@@ -0,0 +1,18 @@
+class C {
+
+  [Int32] hidden $val
+
+   C($v) {
+      $this.val = $v
+   }
+}
+
+
+$obj_1 = [C]::new(4)
+$obj_2 = [C]::new(2)
+$obj_3 = [C]::new(4)
+
+$obj_4 = $obj_1
+
+$obj_1 -eq $obj_4  # $obj_1 and $obj_4 refer to the same object, really
+$obj_1 -eq $obj_3  # $obj_1 and $obj_3 refer to different objects
