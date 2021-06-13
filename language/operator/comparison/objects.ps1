69fdb8eec42097460e1606b4e2953fe9988901ee
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-06-13 17:58:07 +0200

    more comment

--- language/operator/comparison/objects.ps1
+++ language/operator/comparison/objects.ps1
@@ -14,5 +14,5 @@ $obj_3 = [C]::new(4)
 
 $obj_4 = $obj_1
 
-$obj_1 -eq $obj_4  # $obj_1 and $obj_4 refer to the same object, really
-$obj_1 -eq $obj_3  # $obj_1 and $obj_3 refer to different objects
+$obj_1 -eq $obj_4  # $obj_1 and $obj_4 refer to the same object   -> operator returns $true
+$obj_1 -eq $obj_3  # $obj_1 and $obj_3 refer to different objects -> operator returns $false
