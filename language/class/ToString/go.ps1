93e3872f0656caea8d538b1a34a28439a80b340b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-08-06 10:52:29 +0200

    + select-string.ps1

--- language/class/ToString/go.ps1
+++ language/class/ToString/go.ps1
@@ -2,3 +2,4 @@
 . ./classWithoutToString.ps1
 . ./callToString.ps1
 . ./stringInterpolation.ps1
+. ./select-string.ps1
---
+++ language/class/ToString/select-string.ps1
@@ -0,0 +1,4 @@
+$obj_with, $obj_without | select-string o
+#
+#  num = 42, text = hello world
+#  classWithoutToString
