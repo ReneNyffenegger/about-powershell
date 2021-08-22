8f958247f17089a68ab0949f09f69de13f178b06
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-08-22 20:19:58 +0200

    fix script

--- cmdlets/itemProperty/remove/registry-value.ps1
+++ cmdlets/itemProperty/remove/registry-value.ps1
@@ -2,13 +2,14 @@ $regKey = 'hkcu:\Software\tq84_removeItemTest'
 
 $null = new-item $regKey
 
-
 set-itemProperty $regKey  num   42
 set-itemProperty $regKey  txt  'Hello world'
 set-itemProperty $regKey  xyz  'Foo, bar, baz'
 
-read-host 'Registry key and values created'
+read-host 'Registry key and values created.'
 
 remove-itemProperty $regKey xyz
+
+read-host 'Value "xyz" removed.'
 
 remove-item $regKey
