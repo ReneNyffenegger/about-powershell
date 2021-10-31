e8fe34d3e4f950c98bab746fe7db0a6fc40bac69
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-10-31 21:16:29 +0100

    + desktop-vs-core.ps1

---
+++ cmdlets/webRequest/invoke/desktop-vs-core.ps1
@@ -0,0 +1,39 @@
+set-strictMode -version 3
+
+function check-eq {
+   param (
+      [string] $value,
+      [string] $expectedDesktopValue,
+      [string] $expectedCoreValue
+   )
+
+   if ($psVersionTable.psEdition -eq 'Desktop') {
+      if ($value -ne $expectedDesktopValue) {
+         write-host "Expected Desktop value was $expectedDesktopValue, but I had $value"
+      }
+
+   }
+   elseif ($psVersionTable.psEdition -eq 'Core') {
+
+      if ($value -ne $expectedCoreValue) {
+         write-host "Expected Core value was $expectedCoreValue, but I had $value"
+      }
+
+   }
+
+}
+
+$url = 'https://renenyffenegger.ch/'
+
+$response         = invoke-webRequest $url
+$baseResponse     = $response.baseResponse
+
+$responseType     =     $response.GetType().FullName
+$baseResponseType = $baseResponse.GetType().FullName
+
+
+
+check-eq     $responseType  'Microsoft.PowerShell.Commands.HtmlWebResponseObject'   'Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject'
+check-eq $baseResponseType  'System.Net.HttpWebResponse'                            'System.Net.Http.HttpResponseMessage'
+
+$baseresponse | member
