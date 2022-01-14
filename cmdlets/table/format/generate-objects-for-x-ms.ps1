10e8a89aeb5728ea0cecbcce11d51b50a7c2bde0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-01-14 06:03:43 +0100

    + .\generate-objects-for-x-ms.ps1

---
+++ cmdlets/table/format/generate-objects-for-x-ms.ps1
@@ -0,0 +1,17 @@
+param (
+   $milliSeconds
+)
+
+$ticks_ = (get-date).ticks
+
+$i = 0
+while ( (get-date).ticks - $ticks_ -lt $milliSeconds * [System.TimeSpan]::TicksPerMillisecond ) {
+   $i++
+   [pscustomobject] @{num = $i; txt = 'abcdefghi' }
+
+   start-sleep -milliSecond 15
+
+}
+
+$i++; [pscustomobject] @{num = $i; txt = 'abcdefghijklmnopqrstuvwxyz' }
+$i++; [pscustomobject] @{num = $i; txt = 'abcdef'                     }
