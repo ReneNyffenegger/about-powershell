5ac9dee0bb7ac2ca61093d3aea914217b508b9af
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-06-03 18:44:52 +0200

    + ExtractStringFromDLL.ps1

---
+++ examples/WinAPI/ExtractStringFromDLL.ps1
@@ -0,0 +1,51 @@
+add-type -typeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class tq84_krnl {
+
+  [DllImport("kernel32.dll",
+      SetLastError = true,
+      CharSet      = CharSet.Ansi
+   )]
+   private static extern IntPtr              LoadLibraryEx
+   (
+     [MarshalAs(UnmanagedType.LPStr)]string  lpFileName,
+      IntPtr                                 hFile,
+      uint                                   dwFlags
+   );
+
+  [DllImport("user32.dll"  ,
+      SetLastError = true,
+      CharSet      = CharSet.Auto
+   )]
+   private static extern int                 LoadString
+   (
+      IntPtr                                 hInstance,
+      int                                    ID,
+      StringBuilder                          lpBuffer,
+      int                                    nBufferMax
+   );
+
+  [DllImport("kernel32.dll",
+      SetLastError = true)]
+  [return: MarshalAs(UnmanagedType.Bool)]
+   private static extern bool                FreeLibrary
+   (
+      IntPtr                                 hModule
+   );
+
+  public static string ExtractStringFromDLL(string file, int number) {
+
+      IntPtr lib = LoadLibraryEx(file, new IntPtr(0L), 32); // 32 = LOAD_LIBRARY_AS_IMAGE_RESOURCE
+      StringBuilder result = new StringBuilder(2048);
+      LoadString(lib, number, result, result.Capacity);
+      FreeLibrary(lib);
+
+      return result.ToString();
+  }
+}
+"@
+
+
